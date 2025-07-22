7e235a321db16c2c46746dd6e0bc94176fe47175
billybee12 <starnote320389@gmail.com>
2025-07-22 16:13:05 +0900

    Initial Commit

---
+++ way/wayApp.swift
@@ -0,0 +1,17 @@
+//
+//  wayApp.swift
+//  way
+//
+//  Created by 김상훈 on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct wayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wayTests/wayTests.swift
@@ -0,0 +1,16 @@
+//
+//  wayTests.swift
+//  wayTests
+//
+//  Created by 김상훈 on 7/22/25.
+//
+
+import Testing
+
+struct wayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
